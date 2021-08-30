76388f2187b5a60c893fc8b067d4a0d34d7ec6de
saohwan <saohwan@github.com>
2021-08-31 00:14:22 +0900

    post게시글 작성

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()  # 텍스트 필드는 길이를 정의할 필요 없다
+
+    created_at = models.DateTimeField()
+
+    # author : 추후 작성 예정
+
+    def __str__(self):
+        return f'[{self.pk}] {self.title}'
--- single_pages/apps.py
+++ single_pages/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SinglePageConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'single_page'
+    name = 'single_pages'
