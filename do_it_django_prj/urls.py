113846cf18f4652a835ff5f4cd02b71d4feb3f3c
saohwan <saohwan@github.com>
2021-09-04 18:08:34 +0900

    대문 페이지 작성

--- do_it_django_prj/urls.py
+++ do_it_django_prj/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('blog/', include('blog.urls')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('', include('single_pages.urls')),
 ]
--- single_pages/views.py
+++ single_pages/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def landing(request):
+    return render(
+        request,  # request 가 text가 ('request') < 되면 안된다.
+        'single_pages/landing.html',
+    )
+
+
+def about_me(request):
+    return render(
+        request,
+        'single_pages/about_me.html',
+    )
