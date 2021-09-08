a0df6a35d024c180b73fc978c44e9c90f6cfe7ad
saohwan <saohwan@github.com>
2021-09-08 23:59:12 +0900

    미리보기와 요약문 추가

--- blog/models.py
+++ blog/models.py
@@ -3,9 +3,13 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
+    hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()  # 텍스트 필드는 길이를 정의할 필요 없다
 
-    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)  # 파일을 업로드할때 형식 지정 , blank=True
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/',
+                                   blank=True)  # 파일을 업로드할때 형식 지정 , blank=True는 비워있어도 된다는 뜻
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d', blank=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
