962a49d1d39ef22cc674d4a1e5c84345c15d69fb
saohwan <saohwan@github.com>
2021-09-16 23:57:25 +0900

    author필드 추가

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 import os
-
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -15,10 +15,10 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    # author : 추후 작성 예정
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  # models.CASCADE 를 하면 작성자가 작성한 게시글 까지 같이 삭제.
 
     def __str__(self):
-        return f'[{self.pk}] {self.title}'
+        return f'[{self.pk}] {self.title} :: {self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'  # 고유의 url 정의
--- blog/tests.py
+++ blog/tests.py
@@ -7,6 +7,23 @@ class TestView(TestCase):
     def setUp(self):
         self.client = Client()
 
+    def navbar_test(self, soup):
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About Me', navbar.text)
+
+        logo_btn = navbar.find('a', text='Saohwan')
+        self.assertEqual(logo_btn.attrs['href'], '/')
+
+        home_btn = navbar.find('a', text='Home')
+        self.assertEqual(home_btn.attrs['href'], '/')
+
+        blog_btn = navbar.find('a', text='Blog')
+        self.assertEqual(blog_btn.attrs['href'], '/blog')
+
+        about_me_btn = navbar.find('a', text='About Me')
+        self.assertEqual(about_me_btn.attrs['href'], '/about_me')
+
     def test_post_list(self):
         # 1.1 포스트 목록 페이지 (post list)를 연다.
         response = self.client.get('/blog/')
@@ -15,11 +32,8 @@ class TestView(TestCase):
         # 1.3 페이지의 타이틀에 Blog 라는 문구가 있다.
         soup = BeautifulSoup(response.content, 'html.parser')
         self.assertIn('Blog', soup.title.text)  # soup.title.text에 'blog' 라는 문구가 있어야한다. soup 으로 접근
-        # 1.4 NavBar 가 있다.
-        navbar = soup.nav
-        # 1.5 Blog, About me 라는 문구가 Navbar 에 있다.
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About', navbar.text)
+
+        self.navbar_test(soup)
 
         # 2.1 게시물이 하나도 없을 때
         self.assertEqual(Post.objects.count(), 0)  # Post 모델에 레코드가 몇개 있는지 아무 겄도 없을때
@@ -65,9 +79,8 @@ class TestView(TestCase):
 
         soup = BeautifulSoup(response.content, 'html.parser')
         # 2.2 포스트 목록 페이지와 똑같은 내비게이션 바가 있다.
-        navbar = soup.nav
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About', navbar.text)
+        self.navbar_test(soup)
+
         # 2.3 첫 번째 포스트의 제목이 웹 브라우저 탭 타이틀에 들어 있다.
         self.assertIn(post_001.title, soup.title.text)
         # 2.4 첫 번째 포스트의 제목이 포스트 영역에 있다.
