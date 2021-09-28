7f2cba7bb5d3f8b62cd8300c1b498a3e12a6edeb
saohwan <saohwan@github.com>
2021-09-28 23:34:46 +0900

    태그 페이지 작성

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('category/<str:slug>/', views.category_page), # 소문자로 category_page한 이유는 함수로 만들겠다는 뜻.
+    path('tag/<str:slug>/', views.tag_page),
+    path('category/<str:slug>/', views.category_page),  # 소문자로 category_page한 이유는 함수로 만들겠다는 뜻.
     path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
 ]
